9a76858567ae18ccc6f11d96f267fb20e2ca811e
Mikael BRUNET <alsc-adaitp21-bmi@ccicampus.fr>
2022-01-02 20:57:29 +0100

    Modification des styles de la fenetre de connexion et création d'une structure "testUnitaire"

---
+++ TestUnitaireBredele/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestUnitaireBredele
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
