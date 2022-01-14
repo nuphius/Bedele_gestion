21367eaf6df0c6701e28b38a4ffde48b01c82303
ARNOLDMathieu <arnold.mathieu@outlook.com>
2022-01-14 10:03:49 +0100

    Modif interface

--- BredeleGestion.Services/GestionLocauxService.cs
+++ BredeleGestion.Services/GestionLocauxService.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text.RegularExpressions;
 
 namespace BredeleGestion.Services
 {
-    public class Class1
+    public class GestionLocauxService
     {
+        
+
     }
 }
