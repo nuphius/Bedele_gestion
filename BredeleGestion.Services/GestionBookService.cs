8837cac42b90c793097434c449acfbb5a10881df
Mikael BRUNET <alsc-adaitp21-bmi@ccicampus.fr>
2022-01-27 10:51:40 +0100

    codage controle des champs réservation

--- BredeleGestion.Services/GestionBookService.cs
+++ BredeleGestion.Services/GestionBookService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,84 @@ namespace BredeleGestion.Services
 {
     public class GestionBookService
     {
+        private List<Activity> _listAdherent = new List<Activity>();
+        private List<BoxPlace> _listPlace = new List<BoxPlace>();
+
+        public GestionBookService()
+        {
+
+        }
+
+        #region LoadActivity
+        /// <summary>
+        /// Récupere toutes les activités de la BDD et retourne un list d'objet activity
+        /// </summary>
+        /// <returns></returns>
+        public List<Activity> LoadActivity()
+        {
+            ConnexionBddService connexionBddService = new ConnexionBddService(RequetSqlService.SELECTACTIVITY, RequetSqlService.TABLEACTIVITY);
+            List<DataRow> listRstBdd = connexionBddService.ExecuteRequet();
+
+            foreach (DataRow activity in listRstBdd)
+            {
+                try
+                {
+                    int id = int.Parse(activity["activid"].ToString());
+
+                    _listAdherent.Add(new Activity { Id = id, Name = activity["activname"].ToString() });
+                }
+                catch (Exception ex)
+                {
+                    LogTools.AddLog(LogTools.LogType.ERREUR, "Problème de convertion des id de la bdd activity" + ex.Message);
+                }
+            }
+
+            return _listAdherent;
+        }
+        #endregion
+
+        public List<BoxPlace> LoadPlace()
+        {
+            ConnexionBddService connexionBddService = new ConnexionBddService(RequetSqlService.SELECTBOX, RequetSqlService.TABLEBOX);
+            List<DataRow> listRstBdd = connexionBddService.ExecuteRequet();
+
+            foreach (DataRow dataRow in listRstBdd)
+            {
+                try
+                {
+                    int id = int.Parse(dataRow["boxid"].ToString());
+                    int place = int.Parse(dataRow["boxcapacity"].ToString());
+
+                    _listPlace.Add(new BoxPlace { Id = id, Name = dataRow["boxname"].ToString(), Place = place});
+                }
+                catch (Exception ex)
+                {
+                    LogTools.AddLog(LogTools.LogType.ERREUR, "Problème de convertion des id ou place de la bdd box" + ex.Message);
+                }
+            }
+            return _listPlace.OrderBy(x => x.Place).ToList();
+        }
+    }
+
+    public class Activity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name.ToString();
+        }
+    }
+    public class BoxPlace
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Place { get; set; }
+
+        public override string ToString()
+        {
+            return Place.ToString();
+        }
     }
 }
--- Bredele_Gestion/viewmodel/ViewBookViewModel.cs
+++ Bredele_Gestion/viewmodel/ViewBookViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using BredeleGestion.Services;
 
@@ -16,26 +17,118 @@ namespace Bredele_Gestion.viewmodel
         ListAdherentService adherentService = new ListAdherentService();
         public ObservableCollection<ListAdherentService> ListCust { get; set; }
 
-        private string _searchName;
+        readonly Regex regex = new Regex("^[0-9]{0,2}$");
 
+        public DateTime SelectedDate { get; set; }
+
+        private string _searchName;
+        private string _hoursEnd;
+        private string _hoursStart;
+        public List<BoxPlace> ListPlaces { get; set; }
+        public List<Activity> ListActivity { get; set; }
+
+        #region SearchName
+        /// <summary>
+        /// proprieté de recherche de nom
+        /// </summary>
         public string SearchName
         {
             get { return _searchName; }
-            set 
-            { 
+            set
+            {
                 _searchName = value;
                 adherentService.SelectCustomers(value);
                 ListCust = adherentService.ListCust;
-                //this.NotifyPropertyChanged(nameof(SearchName));
             }
         }
+        #endregion
+
+        #region HoursStart
+        /// <summary>
+        /// proprieté bindé pour l'heure de début
+        /// </summary>
+        public string HoursStart
+        {
+            get { return _hoursStart; }
+            set
+            {
+                if (value != "")
+                {
+                    if (regex.IsMatch(value) && int.Parse(value) < 25)
+                    {
+                        _hoursStart = value;
+                    }
+                    else
+                    {
+                        if (!string.IsNullOrEmpty(_hoursStart) && !string.IsNullOrEmpty(value))
+                        {
+                            value = HoursStart;
+                        }
+                        else
+                        {
+                            _hoursStart = string.Empty;
+                            value = string.Empty;
+                        }
+                    }
+                }
+                else
+                {
+                    _hoursStart = string.Empty;
+                    value = string.Empty;
+                }
+
+                this.NotifyPropertyChanged(nameof(HoursStart));
+            }
+        }
+        #endregion
+
+        #region HoursEnd
+        /// <summary>
+        /// propriété bindé pour l'heure de fin
+        /// </summary>
+        public string HoursEnd
+        {
+            get { return _hoursEnd; }
+            set
+            {
+                if (value != "")
+                {
+                    if (regex.IsMatch(value) && int.Parse(value) < 25)
+                    {
+                        _hoursEnd = value;
+                    }
+                    else
+                    {
+                        if (!string.IsNullOrEmpty(_hoursEnd) && !string.IsNullOrEmpty(value))
+                        {
+                            value = HoursEnd;
+                        }
+                        else
+                        {
+                            _hoursEnd = string.Empty;
+                            value = string.Empty;
+                        }
+                    }
+                }
+                else
+                {
+                    _hoursEnd = string.Empty;
+                    value = string.Empty;
+                }
+
+                this.NotifyPropertyChanged(nameof(HoursEnd));
+            }
+        }
+        #endregion
+
 
         public event PropertyChangedEventHandler PropertyChanged;
         public ViewBookViewModel()
         {
+            GestionBookService bookService = new GestionBookService();
             SearchName = "";
-            //adherentService.SelectCustomers();
-            //ListCust = adherentService.ListCust;
+            ListActivity = bookService.LoadActivity();
+            ListPlaces = bookService.LoadPlace();
         }
 
         #region NotifyPropertyChanged
